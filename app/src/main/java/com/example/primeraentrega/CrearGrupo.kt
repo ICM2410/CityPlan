cf19efc86df085332b1969dade8f0c547daef28b
KS-TY <juancasl2015@gmail.com>
2024-02-17 15:36:01 -0500

    añadi crear grupo

---
+++ app/src/main/java/com/example/primeraentrega/CrearGrupo.kt
@@ -0,0 +1,11 @@
+package com.example.primeraentrega
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearGrupo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_grupo)
+    }
+}
