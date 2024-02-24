55a5c9479f8315245587cd0ae19bf4b90562dd99
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-02-23 21:57:09 -0500

    hice cambios para hacer mis pantallas

---
+++ app/src/main/java/com/example/primeraentrega/EditarPlanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.primeraentrega
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarPlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_plan)
+    }
+}
