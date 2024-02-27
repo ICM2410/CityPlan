8ab4ad24636a67b21588c0f4f6fd4f16665e7716
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-02-27 01:22:46 -0500

    Puse el boton de galeria

--- app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
+++ app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
@@ -1,11 +1,41 @@
 package com.example.primeraentrega
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.primeraentrega.databinding.ActivityGaleriaBinding
+
 
 class GaleriaActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityGaleriaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_galeria)
+        binding= ActivityGaleriaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tomarFoto.setOnClickListener {
+            takePicture()
+        }
     }
+
+    private val CAMERA_REQUEST = 1888 // Constante para identificar la solicitud de la cámara
+
+    private fun takePicture() {
+        // Intent para abrir la aplicación de la cámara
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            // Iniciar la actividad de la cámara y esperar el resultado
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST)
+        } catch (e: ActivityNotFoundException) {
+            // Si no se encuentra ninguna actividad para manejar la acción de la cámara
+            Log.e("PERMISSION_APP", "No se pudo abrir la aplicación de la cámara: " + e.message)
+        }
+    }
+
+
 }
