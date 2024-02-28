150fda4fd1e0a9be79074e82154e18004cc1e4f5
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-02-28 09:06:47 -0500

    puse todo en strings

--- app/src/main/java/com/example/primeraentrega/FijarPuntoMapaActivity.kt
+++ app/src/main/java/com/example/primeraentrega/FijarPuntoMapaActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class FijarPuntoMapaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fijar_punto_mapa)
+
     }
 }
