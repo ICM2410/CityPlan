4bd24d394dd7af5bd01d47aeee21f6e1ae8f34df
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-02-27 23:05:03 -0500

    cambie unas cosas para que la camara sirva

--- app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
+++ app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
@@ -2,12 +2,17 @@ package com.example.primeraentrega
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import com.example.primeraentrega.databinding.ActivityGaleriaBinding
 import java.io.File
@@ -17,9 +22,9 @@ class GaleriaActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityGaleriaBinding
     private lateinit var uriCamera : Uri
+    private val REQUEST_CAMERA_PERMISSION = 100
 
-    val getContentCamera = registerForActivityResult(
-        ActivityResultContracts.TakePicture(),
+    val getContentCamera = registerForActivityResult(ActivityResultContracts.TakePicture(),
         ActivityResultCallback {
             if(it){
                 loadImage(uriCamera)
@@ -40,7 +45,11 @@ class GaleriaActivity : AppCompatActivity() {
     }
 
     private fun takePicture() {
-        getContentCamera.launch(uriCamera)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            getContentCamera.launch(uriCamera)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        }
     }
 
     private fun loadImage(uri : Uri?) {
@@ -48,4 +57,18 @@ class GaleriaActivity : AppCompatActivity() {
         val bitmap = BitmapFactory.decodeStream(imageStream)
         binding.imageView5.setImageBitmap(bitmap)
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso de cámara concedido, puedes lanzar la actividad de la cámara
+                getContentCamera.launch(uriCamera)
+            } else {
+                // Permiso de cámara denegado, puedes mostrar un mensaje al usuario o tomar alguna acción adicional
+                Toast.makeText(this, "Permiso de cámara denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
