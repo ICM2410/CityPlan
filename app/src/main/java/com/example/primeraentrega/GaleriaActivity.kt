05578bb4b3c9c78d11f3fa6c5f86806a2597e2c0
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-02-27 21:51:40 -0500

    correccion pantalla

--- app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
+++ app/src/main/java/com/example/primeraentrega/GaleriaActivity.kt
@@ -2,40 +2,50 @@ package com.example.primeraentrega
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.util.Log
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.example.primeraentrega.databinding.ActivityGaleriaBinding
+import java.io.File
 
 
 class GaleriaActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityGaleriaBinding
+    private lateinit var uriCamera : Uri
+
+    val getContentCamera = registerForActivityResult(
+        ActivityResultContracts.TakePicture(),
+        ActivityResultCallback {
+            if(it){
+                loadImage(uriCamera)
+            }
+        })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityGaleriaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val file = File(getFilesDir(), "picFromCamera");
+        uriCamera = FileProvider.getUriForFile(baseContext,baseContext.packageName + ".fileprovider", file)
+
         binding.tomarFoto.setOnClickListener {
             takePicture()
         }
     }
 
-    private val CAMERA_REQUEST = 1888 // Constante para identificar la solicitud de la cámara
-
     private fun takePicture() {
-        // Intent para abrir la aplicación de la cámara
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            // Iniciar la actividad de la cámara y esperar el resultado
-            startActivityForResult(takePictureIntent, CAMERA_REQUEST)
-        } catch (e: ActivityNotFoundException) {
-            // Si no se encuentra ninguna actividad para manejar la acción de la cámara
-            Log.e("PERMISSION_APP", "No se pudo abrir la aplicación de la cámara: " + e.message)
-        }
+        getContentCamera.launch(uriCamera)
     }
 
-
+    private fun loadImage(uri : Uri?) {
+        val imageStream = getContentResolver().openInputStream(uri!!)
+        val bitmap = BitmapFactory.decodeStream(imageStream)
+        binding.imageView5.setImageBitmap(bitmap)
+    }
 }
