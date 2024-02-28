2a523075b13f60af6c44859326c4dcf70c7a3320
Juan José Gómez Arenas <juanjogomezarenas1@gmail.com>
2024-02-27 22:07:30 -0500

    Se agrego funcionalidad de Leer contactos y pedir permiso para acceder a ellos

--- app/src/main/java/com/example/primeraentrega/AgregarContactosActivity.kt
+++ app/src/main/java/com/example/primeraentrega/AgregarContactosActivity.kt
@@ -1,11 +1,60 @@
 package com.example.primeraentrega
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.provider.ContactsContract
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.primeraentrega.adapters.ContactsAdapter
+import com.example.primeraentrega.databinding.ActivityAgregarContactosBinding
 
 class AgregarContactosActivity : AppCompatActivity() {
+
+    //Permission val
+    val getContactsPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        ActivityResultCallback {
+            updateUI(it)
+        })
+
+    private lateinit var binding: ActivityAgregarContactosBinding
+    private lateinit var adapter : ContactsAdapter
+    val projection = arrayOf(ContactsContract.Profile._ID, ContactsContract.Profile.DISPLAY_NAME_PRIMARY)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_agregar_contactos)
+        binding = ActivityAgregarContactosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        adapter = ContactsAdapter(this, null, 0)
+        binding.listaContactos.adapter=adapter
+        permissionRequest()
+    }
+
+    fun permissionRequest(){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_DENIED){
+            if(shouldShowRequestPermissionRationale(android.Manifest.permission.READ_CONTACTS)){
+                Toast.makeText(this, "The app requires access to the contacts", Toast.LENGTH_LONG).show()
+            }
+            getContactsPermission.launch(android.Manifest.permission.READ_CONTACTS)
+        }else{
+            updateUI(true)
+        }
+    }
+
+    fun updateUI(contacts : Boolean){
+        if(contacts){
+            //Permission Granted
+            var cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, projection, null, null, null)
+            adapter.changeCursor(cursor)
+
+        }else {
+            //Permission Denied
+
+        }
     }
 }
